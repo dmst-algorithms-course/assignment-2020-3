0f7aeedc4cf73d80221526deb202f94447e54f9f
Panos Louridas <louridas@gmail.com>
2020-05-22 22:39:25 +0300

    clarifications and helpers update

--- mpl_draw.py
+++ mpl_draw.py
@@ -10,6 +10,8 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--text", action="store_true",
+                    help="display circle numbers")
 parser.add_argument("input_file", help="input file")
 parser.add_argument("output_file", help="output_file")
 args = parser.parse_args()
@@ -20,25 +22,28 @@ min_y = sys.maxsize
 max_y = -sys.maxsize
 
 circles = []
+texts = []
 segments = []
 
 with open(args.input_file) as input_file:
     for line in input_file:
-        coords = [ float (x) for x in line.split() ]
-        if len(coords) == 3: # circle
-            min_x = min(min_x, coords[0]-coords[2])
-            min_y = min(min_y, coords[1]-coords[2])
-            max_x = max(max_x, coords[0]+coords[2])
-            max_y = max(max_y, coords[1]+coords[2])            
-            circles.append(Circle(coords[:2], coords[2],
+        parts = [ float (x) for x in line.split() ]
+        if len(parts) == 3: # circle
+            min_x = min(min_x, parts[0]-parts[2])
+            min_y = min(min_y, parts[1]-parts[2])
+            max_x = max(max_x, parts[0]+parts[2])
+            max_y = max(max_y, parts[1]+parts[2])            
+            circles.append(Circle(parts[:2], parts[2],
                                   edgecolor='black', facecolor='none',
                                   linewidth=1))
-        elif len(coords) == 4: # segment
-            min_x = min([min_x, *coords[::2]])
-            min_y = min(min_y, *coords[1::2])
-            max_x = max([max_x, *coords[::2]])
-            max_y = max(max_y, *coords[1::2])            
-            segments.append((coords[:2], coords[2:]))
+            if args.text:
+                texts.append((*parts[:2], len(circles)))
+        elif len(parts) == 4: # segment
+            min_x = min([min_x, *parts[::2]])
+            min_y = min(min_y, *parts[1::2])
+            max_x = max([max_x, *parts[::2]])
+            max_y = max(max_y, *parts[1::2])            
+            segments.append((parts[:2], parts[2:]))
 
 fig, ax = plt.subplots()
 ax.set_xlim(min_x - 1, max_x + 1)
@@ -48,6 +53,9 @@ sc.set_capstyle('round')
 ax.add_collection(sc)
 cc = mc.PatchCollection(circles, match_original=True)
 ax.add_collection(cc)
+for text in texts:
+    print(text[0], text[1], text[-1])
+    ax.text(text[0], text[1], str(text[-1]))
 ax.set_aspect('equal')
 ax.axis('off')
 plt.tight_layout()
